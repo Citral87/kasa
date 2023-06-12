0fdcc455d15b303828d7f971963689848917e997
Citral87 <thierry.salzedas@gmail.com>
2023-06-12 22:53:31 +0200

    update
    commentaires

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,57 +6,51 @@ import "../assets/css/about.css";
 import photo from "../assets/data/photo2.jpg";
 
 function About() {
-  const [isOpen1, setIsOpen1] = useState(false);
-  const [isOpen2, setIsOpen2] = useState(false);
-  const [isOpen3, setIsOpen3] = useState(false);
-  const [isOpen4, setIsOpen4] = useState(false);
+  const [collapseItems, setCollapseItems] = useState([
+    {
+      title: "Fiabilité",
+      content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+      isOpen: false
+    },
+    {
+      title: "Respect",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+      isOpen: false
+    },
+    {
+      title: "Service",
+      content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+      isOpen: false
+    },
+    {
+      title: "Sécurité",
+      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+      isOpen: false
+    }
+  ]);
+
+  const toggleItem = index => {
+    setCollapseItems(
+      collapseItems.map((item, i) =>
+        i === index ? { ...item, isOpen: !item.isOpen } : item
+      )
+    );
+  };
 
   return (
     <div className="About">
       <Header />
       <img className="photo2" src={photo} alt="" />
-      <Collapse className="collapse-content_about"
-        title="Fiabilité"
-        isOpen={isOpen1}
-        toggle={() => setIsOpen1(!isOpen1)}
-      >
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes.
-      </Collapse>
-
-      <Collapse className="collapse-content_about"
-        title="Respect"
-        isOpen={isOpen2}
-        toggle={() => setIsOpen2(!isOpen2)}
-      >
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </Collapse>
-
-      <Collapse
-        title="Service"
-        isOpen={isOpen3}
-        toggle={() => setIsOpen3(!isOpen3)}
-      >
-        Nos équipes se tiennent à votre disposition pour vous fournir une
-        expérience parfaite. N'hésitez pas à nous contacter si vous avez la
-        moindre question.
-      </Collapse>
-
-      <Collapse
-        title="Sécurité"
-        isOpen={isOpen4}
-        toggle={() => setIsOpen4(!isOpen4)}
-      >
-        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
-        les voyageurs, chaque logement correspond aux critères de sécurité
-        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-        locataire, cela permet à nos équipes de vérifier que les standards sont
-        bien respectés. Nous organisons également des ateliers sur la sécurité
-        domestique pour nos hôtes.
-      </Collapse>
+      {collapseItems.map((item, i) => (
+        <Collapse
+          key={i}
+          title={item.title}
+          isOpen={item.isOpen}
+          toggle={() => toggleItem(i)}
+        >
+          {item.content}
+        </Collapse>
+      ))}
       <Footer />
     </div>
   );
